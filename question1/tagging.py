30c396b47e2699417ea0cbaf7479d6856ec3f357
Aryanvir <vir.aryan.2007@gmail.com>
2023-08-07 17:15:47 +0530

    new file:   question1/__pycache__/tagging.cpython-310.pyc 	new file:   question1/tagging.py 	new file:   yolov8m.pt

---
+++ question1/tagging.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+
+class Tagging:
+    def __init__(self, source):
+        self.model = YOLO("yolov8m.pt")
+        self.source = source
+
+    def tag_images(self):
+        """Tags each image using YOLOv8
+
+        Returns:
+            None
+        """
+
+        return self.model.predict(self.source, conf=0.375)
